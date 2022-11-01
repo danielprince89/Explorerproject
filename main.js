cac4e016129912f4067419a97bdac29aed98bd18
TechDan <rotimiayodeji65@gmail.com>
2022-11-01 15:25:26 +0100

    harmburger menu

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+
+// es 5
+
+// console.log("send me your account number")
+
+// var writeup = "na me be the Don here"
+
+
+
+var harmburgerdiv = document.querySelector(".harmburger")
+var mobileLinks = document.querySelector(".mobile-links-holder")
+var backdrop = document.querySelector(".backdrop")
+// console.log(harmburgerdiv)
+
+function codedtunez() {
+    harmburgerdiv.classList.toggle("showburger")
+    mobileLinks.classList.toggle("show-mobile-links-holder")
+    backdrop.classList.toggle("show-backdrop")
+}
